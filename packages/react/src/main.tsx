13e2feb77efe0352469b4d4244f3ef326310b798
takuma-ru <kanekotakuma@team-lab.com>
2025-07-15 14:00:39 +0900

    ✨ Add useIsClient export for client-side detection

--- packages/react/src/main.tsx
+++ packages/react/src/main.tsx
@@ -1,4 +1,5 @@
 export { ChatoraWrapper } from "./components/ChatoraWrapper";
+export { useIsClient } from "./hooks/useIsClient";
 export type { toReactEmits } from "./types/main";
 export { disableError } from "./utils/disableError";
 export { hastToJsx } from "./utils/hastToJsx";
